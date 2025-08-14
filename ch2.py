9abfb1c3979a773aeb91610619043a3887aeb0f9
titima0369 <fatima.ezzahra.lahouani00@gmail.com>
2025-08-14 03:18:52 +0100

    Update ch2.py

---
+++ ch2.py
@@ -0,0 +1,13 @@
+user_input = input("Enter a string: ")
+
+
+result = ""
+
+
+for i in range(len(user_input)):
+   
+    if i == 0 or user_input[i] != user_input[i-1]:
+        result += user_input[i]
+
+
+print("String with duplicates removed:", result)
